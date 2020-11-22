f91b6bd2e312cda33aafd7b7a356dc8041ce6994
ujihisa <ujihisa@users.noreply.github.com>
2020-11-22 15:50:50 -0800

    Dummy messages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import React, { useState  } from 'react';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={(event) => setMessages([...messages, 'hello']) }>
+          Reload
+        </button>
+        <ul>
+          {
+            messages.map((message) =>
+              <li>{ message }</li>
+            )
+          }
+        </ul>
+        <br/>
         <a
           className="App-link"
           href="https://reactjs.org"
