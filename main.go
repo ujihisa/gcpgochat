b37861b889a22e9faa4ff577e04dd412c7943870
ujihisa <ujihisa@users.noreply.github.com>
2020-11-22 20:44:23 -0800

    Provide random messages

--- main.go
+++ main.go
@@ -2,11 +2,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 )
+
+type message struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Body string `json:"body"`
+}
+
+type response struct {
+	Messages []message `json:"messages"`
+}
 
 func main() {
 	log.Print("starting server...")
@@ -27,9 +39,9 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	id := rand.Int()
+	body := fmt.Sprintf("hello %v", id)
+	x, _ := json.Marshal(response{[]message{message{id, "random", body}}})
+	fmt.Fprintf(w, string(x))
 }
